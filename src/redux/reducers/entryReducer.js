8cf219ac6a891713992c4e951cec27e089b3ceaa
Kevin J Dexter <kevin.j.dexter@gmail.com>
2018-06-25 17:51:05 -0500

    add values and functions to entry redux files

--- src/redux/reducers/entryReducer.js
+++ src/redux/reducers/entryReducer.js
@@ -12,13 +12,25 @@ const currentEntryId = (state = null, action) => {
   }
 }
 
-// all entries for use by admin to track various activities
-// (ex. # of entries with comments, or # of incomplete entries)
+// all entries to display on journal page
 const allEntries = (state = [], action) => {
   switch(action.type) {
     case "SET_ALL_ENTRIES":
       return action.payload;
     case "UNSET_ALL_ENTRIES":
+      return [];
+    default:
+      return state;
+  }
+}
+
+// all entries to display on admin page for tracking purposes
+// (ex. # of entries with comments, # of incomplete entries, etc);
+const adminPageEntries = (state = [], action) => {
+  switch(action.type) {
+    case "SET_ADMIN_PAGE_ENTRIES":
+      return action.payload;
+    case "UNSET_ADMIN_PAGE_ENTRIES":
       return [];
     default:
       return state;
@@ -41,4 +53,5 @@ export default combineReducers ({
   isLoading,
   currentEntryId,
   allEntries,
+  adminPageEntries,
 });
