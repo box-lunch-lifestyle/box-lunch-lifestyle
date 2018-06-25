dab8c72c3a72d968b2edfbff517a87090024d762
Kevin J Dexter <kevin.j.dexter@gmail.com>
2018-06-25 15:34:36 -0500

    initiate new sagas

---
+++ src/redux/sagas/entrySaga.js
@@ -0,0 +1,8 @@
+import { put, takeEvery } from 'redux-saga/effects';
+
+function* entrySaga() {
+  //yield takeEvery(ACTION, functionCall);
+  console.log('placeholder in entrySaga');
+};
+
+export default entrySaga;
---
+++ src/redux/sagas/positiveMessageSaga.js
@@ -0,0 +1,8 @@
+import { put, takeEvery } from 'redux-saga/effects';
+
+function* positiveMessageSaga() {
+  //yield takeEvery(ACTION, functionCall);
+  console.log('placeholder in positiveMessageSaga');
+};
+
+export default positiveMessageSaga;
