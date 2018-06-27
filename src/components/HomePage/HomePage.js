3c45f7738bbbd8e39280b2c6506daa24a11138d6
Kevin J Dexter <kevin.j.dexter@gmail.com>
2018-06-27 15:24:59 -0500

    message added

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
+import Header from '../Header/Header';
 
 class HomePage extends Component {
   render() {
     return (
       <div>
-        <img alt="Hero guy" />
+        <p>Get pumped up (not permanent)</p>
+        <img alt="Hero guy (image coming)" />
         <br />
         <Button color="primary" variant="contained" >TIMER</Button>
         <br />
         <Button color="primary" variant="contained" >MILESTONES</Button>
         <br />
-        <Button color="primary" variant="contained" >JOURNAL</Button>
+        <Button color="primary" variant="contained" >NOTES TO SELF</Button>
       </div>
     )
   }
