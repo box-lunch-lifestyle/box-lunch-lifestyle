894de1a9efd8e57ba8c70a154acda262a016905a
Torben Jepsen <torbenjepsen@outlook.com>
2018-07-02 16:27:43 -0500

    get is working for journal view, edit works as well

--- src/components/JournalItem/JournalItem.js
+++ src/components/JournalItem/JournalItem.js
@@ -6,30 +6,57 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Edit from '@material-ui/icons/Edit';
 import Grid from '@material-ui/core/Grid';
+import moment from 'moment';
+import { connect } from 'react-redux';
+
 
 class JournalItem extends Component {
-
-    handleClick = (pageLink) => () => {
-        this.props.history.push(pageLink);
+    constructor(props){
+        super(props);
+        this.state = {
+            updatedComment: {
+              date_posted: this.props.comment.date_posted,
+              comment: '',
+            }
+        }
     }
 
+    handleChange = propertyName => event => {
+        this.setState({
+          updatedComment: {
+            ...this.state.updatedComment,
+            [propertyName]: event.target.value,
+          }
+        });
+    }
+
+    handleEditComment = event => {
+        this.props.dispatch({
+          type: 'FETCH_PUT_COMMENT',
+          payload: this.state.updatedComment,
+          id: this.props.comment.id,
+        })
+      }
+
     render() {
+        const date = moment(this.props.comment.date_posted).format("MMMM Do YYYY");
         return (
 
             <Grid container alignItems={'stretch'} justify={'center'} direction={'column'} spacing={16}>
                 <Grid item>
                     <Paper>
                         <Typography variant="headline" component="h3">
-                            <div className="entryDate">june 27th</div>
+                            <div className="entryDate">{date}</div>
                         </Typography>
                         <Typography component="p">
                             <TextField
                                 id="textarea"
-                                label="i wrote 3 haikus. edit icon will be on the right"
+                                label={this.props.comment.comment}
+                                onChange={this.handleChange('comment')}
                                 fullWidth
                                 margin="normal"
                             />
-                            <Button><Edit className="editButton" /></Button>
+                            <Button ><Edit onClick={() => this.handleEditComment()} className="editButton" /></Button>
                         </Typography>
                     </Paper>
                 </Grid>
@@ -38,4 +65,4 @@ class JournalItem extends Component {
     }
 }
 
-export default JournalItem;
+export default connect()(JournalItem);
