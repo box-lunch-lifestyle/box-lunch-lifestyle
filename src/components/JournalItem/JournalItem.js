ef1b3dd03c2522e4633832a3ee34129a4a5b63fe
Kevin J Dexter <kevin.j.dexter@gmail.com>
2018-07-05 10:37:37 -0500

    modify button colors to reflect theme

--- src/components/JournalItem/JournalItem.js
+++ src/components/JournalItem/JournalItem.js
@@ -94,8 +94,8 @@ class JournalItem extends Component {
         title: 'Are you sure?',
         type: 'warning',
         showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#BB221C',
+        confirmButtonColor: '#c82027',
+        cancelButtonColor: '#5f5f5f',
         confirmButtonText: 'Yes, delete it!'
       }).then((result) => {
         if (result.value) {
