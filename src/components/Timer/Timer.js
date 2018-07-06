c1c9c8af12f5f094c299bf1045b75f5ccd4ff622
Cory Booth <Rodimus2005@hotmail.com>
2018-07-06 13:47:40 -0500

    font refixes

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -133,11 +133,11 @@ class TimerPage extends Component {
 
     if (this.props.timer.currentRound === 'food') {
       messageBar = <div className="timerMessageBar">
-        <h2>Enjoy your food.</h2>
+        <h2>CHEW. NOTICE. REFUEL</h2>
       </div>
     } else if (this.props.timer.currentRound === 'life') {
       messageBar = <div className="timerMessageBar">
-        <h2>Enjoy your self.</h2>
+        <h2>DO THIS FOR YOU.</h2>
       </div>
     }
 
