ec1d5e966ad4ddac0b509b88a9fbdb5a05da2174
Cory Booth <Rodimus2005@hotmail.com>
2018-07-06 15:37:02 -0500

    stick figure guy

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -52,7 +52,7 @@ class HomePage extends Component {
       content = (
         <div className={classes.root}>
            <Header title="Box Lunch Lifestyle" />
-          <Grid container alignItems={'center'} justify={'center'} direction={'row'} style={{ marginTop: 20 , marginBottom: 40 }}>
+          <Grid container alignItems={'center'} justify={'center'} direction={'row'} style={{ marginTop: 20 , marginBottom: 60 , marginLeft: 25}}>
             <Grid item xs={4} >
               <h2>Today is your day.</h2>
             </Grid>
@@ -60,17 +60,17 @@ class HomePage extends Component {
               <img src='images/head.png' alt="Hero guy" />
             </Grid>
           </Grid>
-          <Grid container alignItems={'center'} justify={'space-between'} direction={'column'} spacing={24}>
-            <Grid item xs={6} style={{ marginBottom: 15 }}>
+          <Grid container alignItems={'center'} justify={'center'} direction={'column'} spacing={24}>
+            <Grid item xs={6} style={{ marginBottom: 30 }}>
             <Button color="primary" variant="contained" onClick={this.handleClick('/timerSelect')} className={classes.button}>TIMER</Button>
           </Grid>
-          <Grid item xs={6} style={{ marginBottom: 15 }}>
+          <Grid item xs={6} style={{ marginBottom: 30 }}>
             <Button color="primary" variant="contained" onClick={this.handleClick('/milestone')} className={classes.button}>MILESTONES</Button>
           </Grid>
-          <Grid item xs={6} style={{ marginBottom: 15 }} >
+          <Grid item xs={6} style={{ marginBottom: 30 }} >
             <Button color="primary" variant="contained" onClick={this.handleClick('/journal')} className={classes.button}>NOTES TO SELF</Button>
           </Grid>
-          <Grid item xs={6} style={{ marginBottom: 15 }}>
+          <Grid item xs={6} style={{ marginBottom: 30 }}>
           <Button color="secondary" variant="contained" onClick={this.logout} className={classes.button}>LOG OUT</Button>
           </Grid>
         </Grid>
